65c13c33f6e49a8c7cf2dcd74db972e86a036546
xXxZypherxXx <sushikun57@yahoo.com>
2023-10-29 07:00:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ function completion(string, callback) {
 ////THIS ENDS CODERIP
 var term = $('.cmdcontent').terminal(commands, {
     animation,
-    greetings: red("Error: Unable to access A.L.E.X.I.O.S.-Terminal, Horus node appears offline. Intercepting nearest rogue communications.") + val1.innerHTML + red("ERROR: Decryption Failure. Un［r|d］efined media codecs not found. Displaying as rawtext.") + blue(RobinMech.innerHTML) + blue(Monoceros.innerHTML) + val4.innerHTML + "30DE terminal currently open. Please type help for a list of commands.",  
+    greetings: red("Error: Unable to access A.L.E.X.I.O.S.-Terminal, Horus node appears offline. Intercepting nearest rogue communications.") + val1.innerHTML + red("ERROR: Decryption Failure. Un［r|d］efined media codecs not found. Displaying as rawtext.") + blue(RobinMech.innerHTML) + val4.innerHTML + "30DE terminal currently open. Please type help for a list of commands.",  
     prompt: prompt(),
     completion: completion,
     // detect iframe codepen preview
